45a64cbe24e48f51aa82b04b627f9d315b8dcfee
Kate Hyrenko <katya.gyrenko@gmail.com>
2023-03-27 21:13:13 +0200

    Homework for lesson 26

---
+++ objects1.js
@@ -0,0 +1,44 @@
+function Accumulator(startingValue) {
+    this.value = startingValue;
+}
+
+Accumulator.prototype.increment = function() {
+    return this.value++;
+}
+
+Accumulator.prototype.decrement = function() {
+    return this.value--;
+}
+
+function CancelableAccumulator(startingValue) {
+    Accumulator.call(this, startingValue);
+    this.startingValue = startingValue;
+}
+
+CancelableAccumulator.prototype = Accumulator.prototype;
+
+CancelableAccumulator.prototype.clear = function() {
+    this.value = this.startingValue;
+    return this.value;
+}
+
+const myAccumulator = new Accumulator(99);
+
+console.log(myAccumulator.increment());
+console.log(myAccumulator.increment());
+console.log(myAccumulator.increment());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+console.log(myAccumulator.decrement());
+
+console.log(myAccumulator.clear());
+console.log(myAccumulator.clear());
+
+
